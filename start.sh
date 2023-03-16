6dd023f8d0ef1f56c645b102e1b10f908168302c
Delirium Technologies <59111455+JonasKorte@users.noreply.github.com>
2023-03-16 10:27:13 +0100

    Added csdosc submodule

--- start.sh
+++ start.sh
@@ -5,15 +5,5 @@ cd Max
 open Eindopdracht.maxpat
 cd ..
 
-echo "Checking python dependencies..."
-
-python3 -m pip install keyboard
-python3 -m pip install pySerial
-python3 -m pip install python-osc
-
-echo "Dependencies installed successfully."
-
-echo "Running Python Script..."
-cd Python
-sudo python3 bridge.py
-cd ..
+cd csdosc
+npm start
